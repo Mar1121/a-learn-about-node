063b2f0b8a41d4f9b2e8cea256c1258f7aa63f7c
Mar1121 <1184044899@qq.com>
2024-02-19 09:42:06 +0800

    中间件

--- registAndUseRouter.js
+++ registAndUseRouter.js
@@ -6,6 +6,8 @@ const router = require('./路由模块/rrouter')
 // 注册路由模块
 // app.use() 函数的作用是用来注册全局中间件
 app.use(router)
+// 给路由模块添加同意的路由前缀
+// app.use('/api',router) 
 
 app.listen(80, function () {
     console.log('Server is running at http://127.0.0.1');
