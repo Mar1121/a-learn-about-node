0d50af022193f5d3e7d1d6748e51b2e65fb1a59c
unknown <1184044899@qq.com>
2024-02-26 09:27:13 +0800

    修改用户信息的接口

--- node服务器端demo/router_handler/userinfo.js
+++ node服务器端demo/router_handler/userinfo.js
@@ -1,5 +1,7 @@
 // 导入数据库操作模块
 const db = require('../db')
+// 导入处理密码的模块
+const bcrypt = require('bcryptjs')
 
 // 获取用户基本信息的处理函数
 exports.getUserInfo = (req, res) => {
@@ -31,5 +33,72 @@ exports.getUserInfo = (req, res) => {
 // 更新用户基本信息的处理函数
 exports.updateUserInfo = (req, res) => {
     // 定义更新用户基本信息的SQL语句
+    const sql = 'update ev_users set ? where id=?'
+    // res.send('ok')
+    db.query(sql, [req.body, req.body.id], (err, results) => {
+        if (err) {
+            return res.cc(err)
+        }
+        if (results.affectedRows !== 1) {
+            return res.cc('更新用户基本信息失败！')
+        }
+        // 成功的结果
+        res.cc('更新用户基本信息成功！', 0)
+    })
+
+}
+
+// 更新用户密码的处理函数
+exports.updatePassword = (req, res) => {
+    // res.send('ok')
+    // 根据id查询用户的信息
+    const sql = 'select * from ev_users where id=?'
+    // 执行根据id查询用户信息的sql语句
+    db.query(sql, req.user.id, (err, results) => {
+        // 执行sql语句失败
+        if (err) {
+            return res.cc(err)
+        }
+        // 判断结果是否存在
+        if (results.length !== 1) {
+            return res.cc('用户不存在！')
+        }
+        // 判断用户的旧密码是否输入正确
+        const compareResult = bcrypt.compareSync(req.body.oldPwd, results[0].password)
+        if (!compareResult) {
+            return res.cc('旧密码输入错误！')
+        }
+        // 更新数据库中的代码
+        const sql = 'update ev_users set password=? where id=?'
+        const newPwd = bcrypt.hashSync(req.body.newPwd, 10)
+        db.query(sql, [newPwd, req.user.id], (err, results) => {
+            // 执行sql语句失败
+            if (err) {
+                return res.cc(err)
+            }
+            if (results.affectedRows !== 1) {
+                return res.cc('更新密码失败！')
+            }
+        })
+        res.cc('更新密码成功')
+    })
+}
+
+// 更新用户头像的处理函数
+exports.updateAvatar = (req, res) => {
+    // 定义更新头像的SQL语句
+    const sql = 'update ev_users set user_pic=? where id=?'
+    // 执行SQL语句
+    db.query(sql, [req.body.avatar, req.user.id], (err, results) => {
+        //
+        if (err) {
+            return res.cc(err)
+        }
+        // 
+        if (results.affectedRows !== 1) {
+            return res.cc('更换头像失败！')
+        }
+        res.cc('更换头像成功', 0)
+    })
     res.send('ok')
 }
--- node服务器端demo/schema/user.js
+++ node服务器端demo/schema/user.js
@@ -5,10 +5,42 @@ const joi = require('joi')
 const username = joi.string().alphanum().min(1).max(10).required()
 const password = joi.string().pattern(/^[\S]{6,12}$/).required()
 
+// 定义id，nickname，email的验证规则
+const id = joi.number().integer().min(1).required()
+const nickname = joi.string().required()
+const user_email = joi.string().email().required()
+
+// 定义avatar头像的验证规则
+const avatar = joi.string().dataUri().required()
+
 // 定义验证注册和登录表单数据的规则对象
 exports.reg_login_schema = {
     body: {
         username,
         password
     }
+}
+
+//更新用户基本信息的验证规则对象 
+exports.update_userinfo_schema = {
+    // 需要对res.body 里面的数据进行验证
+    body: {
+        id,
+        nickname,
+        email: user_email
+    }
+}
+
+// 验证规则-更新密码
+exports.update_password_schema = {
+    body: {
+        oldPwd: password,
+        newPwd: joi.not(joi.ref('oldPwd')).concat(password)
+    }
+}
+
+
+// 验证规则对象-更新头像
+exports.update_avatar_schema = {
+    body: { avatar }
 }
